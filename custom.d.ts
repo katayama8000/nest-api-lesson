eec4dc684a3db6cc3c60e785d0db58616a9b1ea7
katayama <tattu.0310@gmail.com>
2022-08-27 18:08:49 +0900

    ✨ user update

---
+++ custom.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { User } from '@prisma/client';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: Omit<User, 'hashedPassword'>;
+  }
+}
